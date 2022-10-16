2182b2ab98b5766cae74af464e2531e4ad5d64c3
carlosEduardo704 <carlos704eduardo@gmail.com>
2022-10-16 10:48:52 -0300

    resolvendo exercicio-10

--- exercicio-10.py
+++ exercicio-10.py
@@ -1,4 +1,7 @@
-
+"""
+Faça um programa que lê a nota da prova de 15 alunos e as amazene em um vetor,
+calcule e imprima a média geral.
+"""
 
 
 from random import randint as R
@@ -6,4 +9,5 @@ from random import randint as R
 notas = [R(15, 100) for i in range(15)]
 
 media = sum(notas) / len(notas)
-print(media)
+
+print(f'A media geral é: {media:.2f}')
